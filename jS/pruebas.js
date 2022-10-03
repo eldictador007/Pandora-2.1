9718d792e1c4e1e0b9ac24c132de81e4b5e1c2f0
eldictador007 <chantilli@protonmail.ch>
2022-10-03 18:00:36 -0300

    se agrego referencia a nueva pagina en inicio

--- jS/pruebas.js
+++ jS/pruebas.js
@@ -1,2 +1,14 @@
 //
 
+let welcome=true
+
+function Objeto(id,nombre,precio,stock){
+    this.id=id
+    this.nombre=nombre
+    this.precio=precio 
+    this.stock=stock
+}
+
+const obj1= new Objeto(1,"Uno",6,53)
+
+console.log(obj1)
